f7975cb1e5fb93623fbd06eb27d8e1af5fb9bdc3
monshi633 <monshi.633@gmail.com>
2024-05-10 19:59:22 +0200

    Disables payments

---
+++ php/paypalPayments.php
@@ -0,0 +1,9 @@
+<?php
+    //Recibir post de paypal
+    //comprobar credenciales
+    //    Came from PayPal.
+      //  Was not altered or corrupted during transmission.
+        //Was intended for you.
+        //Contains a valid signature.
+    //devolver 200
+?>
